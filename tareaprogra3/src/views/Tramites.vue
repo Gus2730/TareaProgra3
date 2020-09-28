95381cbca1a08e97882e51eb9ca220da138ecde1
Gus2730 <gus2730@gmail.com>
2020-09-28 17:22:57 -0600

    cambios
    Cambios

--- tareaprogra3/src/views/Tramites.vue
+++ tareaprogra3/src/views/Tramites.vue
@@ -22,11 +22,13 @@
                     id="busqueda"
                     class="btn btn-primary"
                     type="button"
-                    @click="traer"
+                    @click="Conseguir"
                   >
                     <span class="fa fa-search"></span>
                   </button>
+                  
                 </div>
+                <div><br></div>
                 <div class="inner-form">
                   <div class="input-field first-wrap">
                     <div class="btn-group">
@@ -92,7 +94,8 @@
         </div>
       </div>
     </div>
-    <div class="btn btn-info">
+    <div><br></div>
+    <div  class="btn btn-info">
       <input
         type="button"
         value="Volver al inicio"
@@ -101,10 +104,109 @@
       />
     </div>
   </div>
- 
 </template>
 
 <script>
 require("@/css/style2.css");
-export default {};
+import store from "vuex";
+export default {
+  name: "Tramites",
+  data() {
+     return {
+    titulo: "Trámites"
+     }
+  },
+  methods: {
+    Conseguir() {
+      var dato;
+      fetch("http://localhost:8099/usuarios", {
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+          Accept: "application/json",
+          Authorization:
+            "bearer " +
+            "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTYwMTMzMjI3OCwiZXhwIjoxNjAxMzY4Mjc4fQ.ocHAbAM6bB_QBKN5Qqzie7gX_2V0qjG_eMpVGOgBNMZlE5sOv9a9LZcRyDnP2qcVpXUFFHip57j4O9SGUb7LAQ",
+        },
+      })
+        .then(function (response) {
+          if (response.status != 200) {
+            Swal.fire({
+              icon: "error",
+              title: "Oops...",
+              text:
+                "Ocurrió un error al ingresar, por favor verifique los datos ingresados o su conexíon a internet!",
+              confirmButtonText: `OK`,
+              timer: 10000,
+            });
+          }
+          return response.json();
+        })
+        .then(function (data) {
+          dato = data;
+          $(document).ready(function () {
+            $("tbody").children().remove();
+
+            $.each(dato, function (i, item) {
+              var fila =
+                "<tr><td>" +
+                dato[i].id +
+                "</td><td>" +
+                dato[i].nombreCompleto +
+                "</td><td>" +
+                dato[i].cedula +
+                "</td><td>" +
+                dato[i].estado +
+                "</td><td>" +
+                dato[i].fechaRegistro +
+                "</td></tr>" +
+                '</td><td><button type="button" v-on:click="imprimir(this)" name="remove" id="' +
+                dato[i].id +
+                '" class="btn btn-danger btn_remove">Editar</button></td></tr>';
+              var btn = document.createElement("TR");
+              btn.innerHTML = fila;
+              document.getElementById("contenido").appendChild(btn);
+            });
+          });
+        })
+        .catch(
+          (error) => console.error("Error:", error)
+          // Swal.fire({
+          //     icon: 'error',
+          //     title: 'Oops...',
+          //     text: 'Ocurrió un error al tratar de ingresar, por favor verifique su conexíon a internet y revise si el servidor se encuentra en linea!',
+          //     confirmButtonText: `OK`,
+          //     timer: 20000
+          // }))
+        )
+        .then((response) => console.log("Success:", response));
+    },
+    volverLogin() {
+      window.location.href = "/";
+    },
+    imprimir(comp) {
+      var button_id = comp.id;
+      alert(button_id);
+    },
+  },created:function(){
+    $(document).ready(function () {
+    $(document).on('click', '.dropdown-item', function () {
+
+        var selected = $(this).attr("id");
+        if (selected == "id") {
+            document.getElementById("busqueda").placeholder = "Ingrese el id del tramite ha buscar";
+        }
+        if (selected == "estado") {
+            document.getElementById("busqueda").placeholder = "Ingrese el estado del tramite ha buscar";
+        }
+        if (selected == "cedula") {
+            document.getElementById("busqueda").placeholder = "Ingrese la cedula del tramite ha buscar";
+        }
+        if (selected == "fecha") {
+            document.getElementById("busqueda").placeholder = "Ingrese la fecha de ingreso del tramite ha buscar";
+        }
+    });;
+
+});
+  }
+};
 </script>
